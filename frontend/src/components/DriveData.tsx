f0cbdcb66a5b71ac1d3370faf03a47003eb27979
im-jking <jamesianking@hotmail.com>
2025-01-20 20:37:19 -0800

    Grid formatting

--- frontend/src/components/DriveData.tsx
+++ frontend/src/components/DriveData.tsx
@@ -18,39 +18,41 @@ export default function DriveData() {
   const getDriveData = async () => {
     await fetch("http://localhost:8000/drive_data?include_trashed=True")
       .then((response) => response.json())
-      .then((response) => setDriveData(response))
+      .then((response) => {
+        setDriveData(response);
+        // console.log(response);
+      })
       .catch(() => console.error("Failed to get Drive data."));
   };
 
-  const items: MenuProps['items'] = [
+  const items: MenuProps["items"] = [
     {
-      key: '1',
+      key: "1",
       label: (
-
         <p>
-          <ToolFilled style={{marginRight: "10px"}}/>
+          <ToolFilled style={{ marginRight: "10px" }} />
           Run the Model
         </p>
-      )
+      ),
     },
     {
-      key: '2',
+      key: "2",
       label: (
         <p>
-          <EditFilled style={{marginRight: "10px"}} />
+          <EditFilled style={{ marginRight: "10px" }} />
           Run Draft Operation
         </p>
-      )
+      ),
     },
     {
-      key: '3',
+      key: "3",
       label: (
         <p>
-          <StarFilled style={{marginRight: "10px"}} />
+          <StarFilled style={{ marginRight: "10px" }} />
           Run Deliverable Preparation
         </p>
-      )
-    }
+      ),
+    },
   ];
 
   useEffect(() => {
@@ -58,13 +60,13 @@ export default function DriveData() {
   }, []);
 
   const handleFileSelect = (id: string) => {
-    console.log(selectedFile)
+    console.log(selectedFile);
     console.log(id);
     setSelectedFile(id);
-  }
+  };
 
   return (
-    <div >
+    <div>
       <h2 style={{ textAlign: "center", scale: "1.3" }}>
         Select a State and School
       </h2>
@@ -76,20 +78,32 @@ export default function DriveData() {
         }}
       >
         <ul style={{ textAlign: "center", alignContent: "center" }}>
-          <Dropdown menu={{items}}>
-            <Button className="lc_bt" size="large" style={{marginBottom: "10px", marginLeft: "-50px"}}>
+          <Dropdown menu={{ items }}>
+            <Button
+              className="lc_bt"
+              size="large"
+              style={{ marginBottom: "10px", marginLeft: "-50px" }}
+            >
               Model Type 1
             </Button>
           </Dropdown>
           <br />
-          <Dropdown menu={{items}}>
-            <Button className="lc_bt" size="large" style={{marginBottom: "10px", marginLeft: "-50px"}}>
+          <Dropdown menu={{ items }}>
+            <Button
+              className="lc_bt"
+              size="large"
+              style={{ marginBottom: "10px", marginLeft: "-50px" }}
+            >
               Model Type 2
             </Button>
           </Dropdown>
           <br />
-          <Dropdown menu={{items}}>
-            <Button className="lc_bt" size="large" style={{marginLeft: "-50px"}}>
+          <Dropdown menu={{ items }}>
+            <Button
+              className="lc_bt"
+              size="large"
+              style={{ marginLeft: "-50px" }}
+            >
               Model Type 3
             </Button>
           </Dropdown>
@@ -101,32 +115,67 @@ export default function DriveData() {
           width: "80%",
           textAlign: "center",
           verticalAlign: "top",
-          marginTop: "25px"
+          marginTop: "25px",
         }}
       >
-        <div style={{ display: "flex" }}>
-          {driveData.map((file) => (
-            <div
-              key={file.id}
-              style={{
-                width: "30%",
-                marginLeft: "5%",
-                height: "100%",
-              }}
-            >
-              {/* <Button style={{all: "unset"}}> */}
-              <Button onClick={() => handleFileSelect(file.id)} color={file.id == selectedFile ? "primary" : "default"} style={{border: "none", scale: "2", cursor: "pointer", boxShadow: "none"}}>
-                {file.kind == "drive#folder" ? (
-                  <FolderOutlined />
-                ) : file.kind == "drive#file" ? (
-                  <FileOutlined />
-                ) : (
-                  <QuestionOutlined style={{ scale: "2" }} />
-                )}
-                <br />
-                {file.name}
-              </Button>
-            </div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+        >
+          {driveData.map((file, index) => (
+            <>
+              <div
+                key={file.id}
+                style={{
+                  width: "20%",
+                  marginLeft: "5%",
+                  height: "100%",
+                  flex: "1",
+                }}
+              >
+                {/* <Button style={{all: "unset"}}> */}
+                {/* <Button onClick={() => handleFileSelect(file.id)} color={file.id == selectedFile ? "primary" : "default"} style={{border: "none", scale: "2", cursor: "pointer", boxShadow: "none"}}> */}
+                <Button
+                  onClick={() => handleFileSelect(file.id)}
+                  style={
+                    file.id == selectedFile
+                      ? {
+                          border: "none",
+                          scale: "1.5",
+                          cursor: "pointer",
+                          boxShadow: "none",
+                          backgroundColor: "#4e6fcb",
+                          color: "white",
+                        }
+                      : {
+                          border: "none",
+                          scale: "1.5",
+                          cursor: "pointer",
+                          boxShadow: "none",
+                        }
+                  }
+                >
+                  {file.kind == "drive#folder" ? (
+                    <FolderOutlined />
+                  ) : file.kind == "drive#file" ? (
+                    <FileOutlined />
+                  ) : (
+                    <QuestionOutlined style={{ scale: "2" }} />
+                  )}
+                  <br />
+                  {file.name}
+                </Button>
+              </div>
+              {(index + 1) % 3 === 0 ? (
+                <div
+                  style={{ flexBasis: "100%", height: "0", marginTop: "5%" }}
+                />
+              ) : (
+                <></>
+              )}
+            </>
           ))}
         </div>
       </div>
